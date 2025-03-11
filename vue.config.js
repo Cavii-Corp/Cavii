8c0c9a1c7699d71ef46d6e215b537e4e3ab428ce
mathiaworms <mathias.123@hotmail.fr>
2025-03-11 22:18:14 +0100

    [INIT] electron-app

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,19 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        appId: "com.Cavii.corp",
+        productName: "Cavii",
+        copyright: "Copyright (C) 2025 Cavii",
+        publish: [
+          {
+            provider: "generic",
+            url: ""
+          }
+        ]
+      },
+      customFileProtocol: "./"
+    }
+  }
+};
